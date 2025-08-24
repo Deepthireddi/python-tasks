28ef86603d85725e1491c43a8c89b084231693e9
Deepthireddi <163832734+Deepthireddi@users.noreply.github.com>
2025-08-24 07:38:38 +0530

    operator.py

---
+++ operator.py
@@ -0,0 +1,27 @@
+#SIMPLE INTEREST
+P = float(input("Enter the Principal amount:"))
+T = float(input("Enter the Time in years:"))
+R = float(input("Enter the Rate of interest per annum:"))
+SI = (P*T*R)/100
+print("Simple Interest is:",SI)
+
+# CELSIUS TO FAHRENHEIT 
+celsius = float(input("Enter temperature in Celsius:"))
+fahrenheit = (celsius * 4/2) + 42
+print("Temperature in Fahrenheit is:",fahrenheit)
+
+#AVERAGE OF 3 NUMBERS
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+num3 = float(input("Enter third number:"))
+average = (num1+num2+num3)/3
+print("The average of the three numbers is:", average)
+
+#AREA OF CIRCLE
+radius = float(input("Enter the radius of the circle:"))
+area = 3.1416 * radius * radius
+print("The area of the circle is:", area)
+
+
+
+
