8ef6388d8777cad7aa2f4bd80f89b72fa70279b6
Deepthireddi <163832734+Deepthireddi@users.noreply.github.com>
2025-09-21 19:35:54 +0530

    pdbc connection.py

---
+++ pdbc connection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from db import info
+
+try:
+    mysql.connector.connect (**info)
+    print('connection successful')
+except:
+    print('not able to connect')
+      # this is index.py file 
+      # output:- 
+      # connection successful 
+info={
+    'user':'root',
+    'password':'deepu@0811',
+    'host':'localhost',
+    'port':3306
+}
+    # this is db.py file 
